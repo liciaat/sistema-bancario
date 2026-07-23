1f1b9fa164fcb3aff844c3e79149aa6df119b72f
Belo <guihbelofontes@gmail.com>
2026-07-22 21:18:35 -0300

    fix: ajuste AccountRequestDTO

--- src/main/java/br/com/ufca/sixsevenpayapi/application/dto/AccountResponseDTO.java
+++ src/main/java/br/com/ufca/sixsevenpayapi/application/dto/AccountResponseDTO.java
@@ -15,18 +15,12 @@ public record AccountResponseDTO(
         String name
 ) {
     public static AccountResponseDTO fromEntity(Account account) {
-        if(account instanceof CheckingAccount){
-            AccountType accountType = AccountType.CHECKING;
-        }else {
-            AccountType accountType = AccountType.SAVINGS;
-        }
-
         return new AccountResponseDTO(
                 account.getId(),
                 account.getAccountNumber(),
                 account.getBalance(),
                 account.getCustomer().isActive(),
-                accountType,
+                account.getAccountType(),
                 account.getCustomer().getName()
         );
 
